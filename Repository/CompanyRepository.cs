5f36bd0128464d95c61ac85b339a29331b0475ba
Adamastor Chimalange <adamastor.chimalange@bravantic.co.ao>
2024-12-17 13:13:34 +0000

    [Feature](adamastor): Create Collection do show all companies

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository {
-    public class CompanyRepository: RepositoryBase<Company>,ICompanyRepository {
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository {
 
         public CompanyRepository(RepositoryContext context) : base(context) { }
 
@@ -19,6 +19,10 @@ namespace Repository {
             FindAll(trackChanges)
             .OrderBy(c => c.Name)
             .ToList();
+
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(x => ids.Contains(x.Id), trackChanges)
+                .ToList();
 
         public Company GetCompany(Guid companyId, bool trackChanges) =>
             FindByCondition(c => c.Id.Equals(companyId), trackChanges)
