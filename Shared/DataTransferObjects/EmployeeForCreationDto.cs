a706bf3b85c3b940d3cd2e77f00bd587e87b4ef1
Adamastor Chimalange <adamastor.chimalange@bravantic.co.ao>
2024-12-20 10:21:33 +0000

    [feature](adamastor): create a abstract record EmployeeForManipulationDto

--- Shared/DataTransferObjects/EmployeeForCreationDto.cs
+++ Shared/DataTransferObjects/EmployeeForCreationDto.cs
@@ -6,17 +6,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.DataTransferObjects {
-    public record EmployeeForCreationDto 
+    public record EmployeeForCreationDto : EmployeeForManipulationDto
     {
-        [Required(ErrorMessage ="Employee name is a required field.")]
-        [MaxLength(30, ErrorMessage ="Maximum length for the Name is 30 characters.")]
-        public string? Name { get; init; }
+        //[Required(ErrorMessage ="Employee name is a required field.")]
+        //[MaxLength(30, ErrorMessage ="Maximum length for the Name is 30 characters.")]
+        //public string? Name { get; init; }
 
-        [Range(18, int.MaxValue, ErrorMessage ="Age is required and it can´t be lower then 18")]
-        public int Age { get; set; }
+        //[Range(18, int.MaxValue, ErrorMessage ="Age is required and it can´t be lower then 18")]
+        //public int Age { get; set; }
         
-        [Required(ErrorMessage ="Position is a required field.")]
-        [MaxLength(20, ErrorMessage ="Maximum length for the Position is 20 characters.")]
-        public string? Position { get; init; }
+        //[Required(ErrorMessage ="Position is a required field.")]
+        //[MaxLength(20, ErrorMessage ="Maximum length for the Position is 20 characters.")]
+        //public string? Position { get; init; }
     }
 }
